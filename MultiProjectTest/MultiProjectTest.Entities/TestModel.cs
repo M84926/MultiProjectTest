4686e18b4d9df6aab2567b0f717d0725dc4d5a57
m84926 <vik@tdc.dk>
2018-12-18 02:00:54 -0800

    initial commit

---
+++ MultiProjectTest/MultiProjectTest.Entities/TestModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MultiProjectTest.Entities
+{
+    public class TestModel
+    {
+        public TestModel()
+        {
+
+        }
+
+        public TestModel(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ MultiProjectTest/MultiProjectTest.Service/TestService.cs
@@ -0,0 +1,18 @@
+﻿using MultiProjectTest.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace MultiProjectTest.Service
+{
+    public class TestService
+    {
+        public List<TestModel> GetTestData()
+        {
+            return new List<TestModel>
+            {
+                new TestModel(1,"Viram"),
+                new TestModel(2,"Diksha"),
+            };
+        }
+    }
+}
